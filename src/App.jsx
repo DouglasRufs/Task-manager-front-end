17a8b5a8c139e8364c65cd79c5f9a77b9cea4d18
Douglas Rufino <douglas.rufino.luz@hotmail.com>
2022-05-03 21:50:57 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Sidebar from "./components/Sidebar";
 import Tasks from "./components/tasks";
+import "./components/StyleComponents/App.scss";
 
 const App = () => {
     return (
-        <>
+        <div className="app-container">
+            <Sidebar />
             <Tasks />
-        </>
+        </div>
     );
 };
 
