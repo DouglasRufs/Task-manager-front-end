d32769406213d1c4017649a9f85389374b008372
Douglas Rufino <douglas.rufino.luz@hotmail.com>
2022-05-04 21:38:26 -0300

    fix : login styles import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 import "./components/StyleComponents/App.scss";
 
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 };
